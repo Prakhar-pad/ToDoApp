cb6c3d3556b8a4761200379b94bb3c3b1bdfe115
Prakhar Agrawal <fzd.prakhar@gmail.com>
2021-02-10 16:27:03 +0530

    Added routes and controllers

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express=require('express');
 const port=8000;
 const app=express();
 
+app.use('/', require('./routes'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.static('Assets'));
+app.use(express.urlencoded());
+
 
 
 app.listen(port, function(err){
