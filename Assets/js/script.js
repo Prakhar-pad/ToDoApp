807053aaf990e5c8c1bd78615a3e67a27e051af6
Prakhar Agrawal <fzd.prakhar@gmail.com>
2021-03-17 17:43:02 +0530

    css modified and jquery added

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,17 @@
-var checkedValue = null; 
-var inputElements = document.getElementsByName('checkbox-id');
-console.log('script file added')
-for(var i=0; inputElements[i]; ++i){
-      if(inputElements[i].checked){
-           checkedValue = inputElements[i].value;
-		   console.log('checkedValue= ',checkedValue)
-           break;
-      }
-	console.log(inputElements[i].checked)
-	console.log(inputElements[i].value)
+function myFunction(x) {
+      if (x.matches) { // If media query matches
+            $( init );
+
+            function init() {
+            
+              // Remove the #todo-container element but leave its contents
+              $('#todo-container').unwrap();
+            }
+
+    } 
 }
+    
+    var x = window.matchMedia("(max-width: 500px)")
+
+    myFunction(x) // Call listener function at run time
+
