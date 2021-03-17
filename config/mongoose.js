a8c12bf14cd5a95f5c7b6c2f2e2413103607a8d5
Prakhar Agrawal <fzd.prakhar@gmail.com>
2021-03-17 13:20:41 +0530

    mongoose file updated

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,10 @@
 const mongoose=require('mongoose');
 
 //connect to the database
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/todo_list_db');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/todo_list_db', {
+	useNewUrlParser: true,
+	useUnifiedTopology:true
+});
 
 //acquire the connection(to check if it's successful)
 const db=mongoose.connection;
