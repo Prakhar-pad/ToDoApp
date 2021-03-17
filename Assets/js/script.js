8d538800dfbc8d92f4b236f363912cce4eef78c4
Prakhar Agrawal <fzd.prakhar@gmail.com>
2021-03-17 18:07:53 +0530

    css modified

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,14 +4,16 @@ function myFunction(x) {
 
             function init() {
             
+                  var content=$('#todo-container').html();
               // Remove the #todo-container element but leave its contents
-              $('#todo-container').unwrap();
+              $('#todo-container').replaceWith('<div id="todo-container-mob">'+ content + '</div>')
+            //   $('#todo-container').unwrap();
             }
 
     } 
 }
     
-    var x = window.matchMedia("(max-width: 500px)")
+    var x = window.matchMedia("(max-width: 500px)");
 
     myFunction(x) // Call listener function at run time
 
