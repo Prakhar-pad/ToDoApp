56a91d690ed9488a4cf0d6e2273949496acb35cd
Prakhar Agrawal <fzd.prakhar@gmail.com>
2021-03-21 19:37:11 +0530

    checkbox cross task

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -17,13 +17,25 @@ function myFunction(x) {
 
     myFunction(x) // Call listener function at run time
 
-// Activate and Deactivate delete button through checkbox
-    function toggleLink(checkBox)
-    {
-        var link = document.getElementById("delete-button");
-    
-        if (checkBox.checked)
-            link.style.pointerEvents = "all";
-        else
-            link.style.pointerEvents = "none";
-    }
+    // Activate and Deactivate delete button through checkbox
+
+    // function toggleLink(checkBox)
+    // {
+    //     var link = document.getElementsByName("delete-button");
+    //     console.log(link);
+    //     if (checkBox.checked)
+    //         link.style.pointerEvents = "all";
+    //     else
+    //         link.style.pointerEvents = "none";
+    // }
+
+  //   function deleteCheckBox() {
+  //     var ul = document.getElementById('collection-list');
+  //     var li = ul.children;
+  //     for (var i=0; i < li.length; i++) {
+  //         if (li[i] && li[i].children[0].children[0].children[0].children[0].checked) {
+  //             li[i].children[0].children[2].style.pointerEvents="all"
+
+  //         }
+  //     }
+  // }
