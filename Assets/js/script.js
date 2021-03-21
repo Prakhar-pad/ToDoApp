d62d917bac1d3457848887c0fb044c855b828df9
Prakhar Agrawal <fzd.prakhar@gmail.com>
2021-03-21 19:56:54 +0530

    checkbox with specific task delete added

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,9 @@
+//for mobile view
 function myFunction(x) {
       if (x.matches) { // If media query matches
             $( init );
 
             function init() { 
-            
-                //   var content=$('#todo-container').html();
-              // Remove the #todo-container element but leave its contents
-            //   $('#todo-container').replaceWith('<div id="todo-container-mob">'+ content + '</div>')
               $('#todo-container').unwrap();
             }
 
@@ -17,25 +14,17 @@ function myFunction(x) {
 
     myFunction(x) // Call listener function at run time
 
-    // Activate and Deactivate delete button through checkbox
+    function deleteCheckBox() {
+      var ul = document.getElementById('collection-list');
+      var li = ul.children;
+      for (var i=0; i < li.length; i++) {
+          if (li[i] && li[i].children[0].children[0].children[0].children[0].checked) {
+              li[i].children[0].children[2].style.pointerEvents="all"
 
-    // function toggleLink(checkBox)
-    // {
-    //     var link = document.getElementsByName("delete-button");
-    //     console.log(link);
-    //     if (checkBox.checked)
-    //         link.style.pointerEvents = "all";
-    //     else
-    //         link.style.pointerEvents = "none";
-    // }
+          }
+          else{
+            li[i].children[0].children[2].style.pointerEvents="none"
 
-  //   function deleteCheckBox() {
-  //     var ul = document.getElementById('collection-list');
-  //     var li = ul.children;
-  //     for (var i=0; i < li.length; i++) {
-  //         if (li[i] && li[i].children[0].children[0].children[0].children[0].checked) {
-  //             li[i].children[0].children[2].style.pointerEvents="all"
-
-  //         }
-  //     }
-  // }
+          }
+      }
+  }
